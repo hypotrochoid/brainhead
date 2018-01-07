d638c692617990fda9a31a0c3916d0a853a3e643
Micah McClimans <micah@infinitenullity.com>
2018-01-07 00:23:45 -0500

    Update FullyConnectedNeuralNet.h
    Basic bugfixes, now successfully compiles

--- FullyConnectedNeuralNet.h
+++ FullyConnectedNeuralNet.h
@@ -93,214 +93,182 @@ namespace NeuralNets {
 	};
 
 
-	template<class size, Eigen::Matrix<double, size::value, 1>& value>
+	template<class size, Eigen::Matrix<double, size::value, 1>& value_p>
 	class MatrixWrapper{
 	public:
-		Eigen::Matrix<double, size::value, 1>& value;
+		static const Eigen::Matrix<double, size::value, 1>& value { value_p };
 	};
 
 
-	template<class dimensions, class ... rem>
-	class NeuralNet {
+//	enum LayerType { InputLayer, LossLayer, FullyConnectedLayer};
 
 //		General Form Layer
 
-		template<class activation_function, class nodes_in, class nodes_out, Eigen::Matrix<double, nodes_in::value, 1>& input_vector>
-		class Layer {
-		public:
-			typedef nodes_out Nodes;
-			Eigen::Matrix<double, nodes_out::value, nodes_in::value> weight_matrix;
-			Eigen::Matrix<double, nodes_out::value, 1> output_vector;
-			Eigen::Matrix<double, nodes_out::value, 1> ip_vector;
-			Eigen::Matrix<double, nodes_in::value, 1> bp_errors;
+	template<class activation_function, class in_nodes, class out_nodes>
+	class FullyConnectedLayer {
+	public:
+		typedef in_nodes input_nodes;
+		typedef out_nodes output_nodes;
 
-			Evaluator<activation_function> evaluator;
-			Differentiator<activation_function> differentiator;
+		Eigen::Matrix<double, output_nodes::value, input_nodes::value> weight_matrix;
+		Eigen::Matrix<double, output_nodes::value, 1> output_vector;
+		const Eigen::Matrix<double, input_nodes::value, 1>& input_vector;
+		Eigen::Matrix<double, output_nodes::value, 1> ip_vector;
+		Eigen::Matrix<double, input_nodes::value, 1> bp_errors;
 
+		Evaluator<activation_function> evaluator;
+		Differentiator<activation_function> differentiator;
 
-			void Evaluate() {
-				ip_vector.noalias() = (weight_matrix * input_vector);
-				output_vector.noalias() = ip_vector.unaryExpr(evaluator);
-			};
+		FullyConnectedLayer(const Eigen::Matrix<double, input_nodes::value, 1>& input) : input_vector(input){};
 
-			Eigen::Matrix<double, nodes_in::value, 1>& Backpropagate(double learning_rate, Eigen::Matrix<double, nodes_out::value, 1> &input) {
-//				weight update
-				ip_vector = ip_vector.unaryExpr(differentiator);
-				bp_errors = weight_matrix.transpose()*((ip_vector.array()*input.array()).matrix());
-				Eigen::Matrix<double, nodes_out::value, nodes_in::value> weight_gradient = learning_rate*(input.array()*ip_vector.array()).matrix()*input_vector.transpose();
-				weight_matrix -= weight_gradient;
-				return bp_errors;
-			}
-
+		Eigen::Matrix<double, output_nodes::value, 1>& Evaluate() const {
+			ip_vector.noalias() = (weight_matrix * input_vector);
+			output_vector.noalias() = ip_vector.unaryExpr(evaluator);
 		};
+
+		Eigen::Matrix<double, input_nodes::value, 1>& Backpropagate (double learning_rate, const Eigen::Matrix<double, output_nodes::value, 1> &input) const {
+//				weight update
+			ip_vector = ip_vector.unaryExpr(differentiator);
+			bp_errors = weight_matrix.transpose()*((ip_vector.array()*input.array()).matrix());
+			Eigen::Matrix<double, output_nodes::value, input_nodes::value> weight_gradient = learning_rate*(input.array()*ip_vector.array()).matrix()*input.transpose();
+			weight_matrix -= weight_gradient;
+			return bp_errors;
+		}
+
+	};
 
 //		Input Layer
-		template<class Nodes>
-		class Layer {
-		public:
-			Eigen::Matrix<double, Nodes::value, 1> output_vector;
+	template<class Nodes>
+	class InputLayer {
+	public:
 
-			void Evaluate() {};
+		typedef Nodes input_nodes;
+		typedef Nodes output_nodes;
 
-			Eigen::Matrix<double, Nodes::value, 1>& Backpropagate(double learning_rate, Eigen::Matrix<double, Nodes::value, 1> &input) {
-				return input;
-			}
+
+		const Eigen::Matrix<double, Nodes::value, 1>& output_vector;
+
+		InputLayer(const Eigen::Matrix<double, Nodes::value, 1>& input) : output_vector(input){};
+
+		Eigen::Matrix<double, Nodes::value, 1>& Evaluate() const {
+			return output_vector;
 		};
 
-		template <class Nodes>
-		using InputLayer = Layer<Nodes>;
+		Eigen::Matrix<double, Nodes::value, 1>& Backpropagate(double learning_rate, const Eigen::Matrix<double, Nodes::value, 1> &input) const {
+			return input;
+		}
+	};
 
 //		Loss Layer
 
-		template<class loss_function, class nodes_in, Eigen::Matrix<double, nodes_in::value, 1>& input_vector>
-		class Layer {
-		public:
-			typedef nodes_in Nodes;
+	template<class loss_function, class nodes_in>
+	class LossLayer {
+	public:
+		typedef nodes_in input_nodes;
+		typedef nodes_in output_nodes;
 
-			Eigen::Matrix<double, nodes_in::value, 1>& output_vector;
-			Eigen::Matrix<double, nodes_in::value, 1> bp_errors;
+		const Eigen::Matrix<double, nodes_in::value, 1>& output_vector;
+		Eigen::Matrix<double, nodes_in::value, 1> bp_errors;
 
-			loss_function<nodes_in> loss;
+		loss_function loss;
 
-			Layer() : output_vector(input_vector){};
+		LossLayer(const Eigen::Matrix<double, nodes_in::value, 1>& input) : output_vector(input){};
 
-			void Evaluate() {};
+		Eigen::Matrix<double, nodes_in::value, 1>& Evaluate() const {};
 
-			Eigen::Matrix<double, nodes_in::value, 1>& Backpropagate(double learning_rate, Eigen::Matrix<double, nodes_in::value, 1>& ground_truth) {
-				return loss.Gradient(ground_truth, input_vector);
-			};
-
-
+		Eigen::Matrix<double, nodes_in::value, 1>& Backpropagate(double learning_rate, const Eigen::Matrix<double, nodes_in::value, 1>& ground_truth) const {
+			return loss.Gradient(ground_truth, output_vector);
 		};
-
-		template <class Nodes, class loss_function, Eigen::Matrix<double, Nodes::value, 1>& input_vector>
-		using LossLayer = Layer<loss_function, Nodes, input_vector>;
+	};
 
 
 
-		template<Layer previous_layer, class loss, class nodes>
-		class layer_stack_inside {
+	template<class ... layerstype>
+	class NeuralNet {
+
+		template<class ... Args>
+		class layer_stack_inside{};
+
+
+		template<class layer_type, class ... rest>
+		class layer_stack_inside<layer_type, rest...> {
 		public:
-			typedef nodes Nodes;
-			typedef typeof(previous_layer) previous_layer_type;
-			typedef previous_layer_type::Nodes input_nodes;
+			layer_type owned_layer;
 
-			LossLayer<input_nodes, loss, previous_layer.output_vector> stack;
-			typedef nodes forward_size;
-			typedef MatrixWrapper<stack.output_vector> output;
-
-			void Evaluate() {
-				stack.Evaluate();
-			};
-
-			Eigen::Matrix<double, input_nodes::value, 1> &Backpropagate(double learning_rate, Eigen::Matrix<double, Nodes::value, 1> &input) {
-				return stack.Backpropagate(learning_rate, input);
-			};
-		};
-
-
-		template<Layer previous_layer, class activation, class nodes, class ... rest>
-		class layer_stack_inside {
-		public:
-			typedef nodes Nodes;
-			typedef typeof(previous_layer) previous_layer_type;
-			typedef previous_layer_type::Nodes input_nodes;
-
-			typedef Layer<activation, input_nodes, Nodes, previous_layer.output_vector> owned_layer_type;
-			owned_layer_type owned_layer;
-
-			typedef layer_stack_inside<owned_layer, rest...> tail_stack_type;
+			typedef layer_stack_inside<rest...> tail_stack_type;
 			tail_stack_type tail_stack;
-			typedef tail_stack_type::forward_size forward_size;
-			typedef tail_stack_type::output output;
+
+			typedef typename tail_stack_type::output_type output_type;
+
+			layer_stack_inside(const Eigen::Matrix<double, layer_type::input_nodes::value, 1>& previous_layer_output) : owned_layer(previous_layer_output), tail_stack(owned_layer.output_vector) {};
 
 
-			void Evaluate() {
+			Eigen::Matrix<double, output_type::output_nodes::value, 1>& Evaluate() const {
 				owned_layer.Evaluate();
-				tail_stack.Evaluate();
+				return tail_stack.Evaluate();
 			};
 
-			Eigen::Matrix<double, input_nodes::value, 1> &Backpropagate(double learning_rate, Eigen::Matrix<double, Nodes::value, 1> &input) {
+			Eigen::Matrix<double, layer_type::input_nodes::value, 1> &Backpropagate(double learning_rate, Eigen::Matrix<double, layer_type::output_nodes::value, 1> &input) const {
 				return owned_layer.Backpropagate(learning_rate, tail_stack.Backpropagate(learning_rate, input));
 			};
 
 		};
 
-		template<class input_nodes, class ... rest>
-		class layer_stack_inside {
+		template<class layer_type>
+		class layer_stack_inside<layer_type> {
 		public:
-			typedef input_nodes Nodes;
 
-			Layer<input_nodes> owned_layer;
+			typedef layer_type output_type;
 
-			typedef layer_stack_inside<owned_layer, rest...> tail_stack_type;
-			typedef tail_stack_type::forward_size forward_size;
-			typedef tail_stack_type::output output;
+			layer_type owned_layer;
 
-			tail_stack_type tail_stack;
+			layer_stack_inside(const Eigen::Matrix<double, layer_type::input_nodes::value, 1>& previous_layer_output) : owned_layer(previous_layer_output) {};
 
-			void Evaluate() {
+			Eigen::Matrix<double, layer_type::output_nodes::value, 1>& Evaluate() const {
 				owned_layer.Evaluate();
-				tail_stack.Evaluate();
+				return owned_layer.output_vector;
 			};
 
-			Eigen::Matrix<double, input_nodes::value, 1> &Backpropagate(double learning_rate, Eigen::Matrix<double, Nodes::value, 1> &input) {
-				return tail_stack.Backpropagate(learning_rate, input);
+			Eigen::Matrix<double, layer_type::input_nodes::value, 1> &Backpropagate(double learning_rate, Eigen::Matrix<double, layer_type::output_nodes::value, 1> &input) const {
+				return owned_layer.Backpropagate(learning_rate, input);
 			};
-
-
 		};
 
-
-		template<class input_nodes, class ... layers>
+		template<class input_layer, class ... layers>
 		class LayerStack {
 		public:
-			typedef layer_stack_inside<input_nodes, layers...> stacktype;
+			typedef layer_stack_inside<input_layer, layers...> stacktype;
 			stacktype stack;
 
-			typedef stacktype::forward_size output_size;
-			typedef stacktype::output output;
+			typedef typename stacktype::output_type::output_nodes output_size;
+			typedef typename input_layer::input_nodes input_size;
 
-			output output_value;
+			Eigen::Matrix<double, input_layer::input_nodes::value, 1> input_vector;
 
-			Eigen::Matrix<double, output_size::value, 1>& Evaluate(){
-				stack.Evaluate();
-				return output_value.value;
+			LayerStack() : stack(input_vector) {};
+
+			Eigen::Matrix<double, output_size::value, 1>& Evaluate(const Eigen::Matrix<double, input_layer::input_nodes::value, 1>& input) const {
+				input_vector = input;
+				return stack.Evaluate();
 			}
 
-			void Backpropagate(double learning_rate, Eigen::Matrix<double, output_size::value, 1>& true_value){
+			void Backpropagate(double learning_rate, const Eigen::Matrix<double, output_size::value, 1>& true_value) const {
 				stack.Backpropagate(learning_rate, true_value);
 			}
-
-			Eigen::Matrix<double, input_nodes::value, 1>& GetInput(){
-				return stack.owned_layer.output_vector;
-			}
-
 
 		};
 
 //		Now for the Net itself!
 
-		typedef LayerStack<dimensions, rem...> layerstype;
-		layerstype layers;
+		typedef LayerStack<layerstype...> stacktype;
+		stacktype layers;
 
-		Eigen::Matrix<double, dimensions::value, 1>& GetInput(){
-			return layers.GetInput();
-		};
-
-		Eigen::Matrix<double, layerstype::output_size::value, 1>& GetOutput(){
-			return layers.output_value.value;
-		};
-
-		Eigen::Matrix<double, layerstype::output_size::value, 1>& Evaluate(Eigen::Matrix<double, dimensions::value, 1>& input_val) {
-			GetInput() = input_val;
-			layers.Evaluate();
-			return GetOutput();
+		Eigen::Matrix<double, stacktype::output_size::value, 1>& Evaluate(const Eigen::Matrix<double, stacktype::input_size::value , 1>& input) const {
+			return layers.Evaluate(input);
 		}
 
-		void Learn(Eigen::Matrix<double, dimensions::value, 1>& input_val, Eigen::Matrix<double, layerstype::output_size::value, 1>& output_val, double learning_rate){
-			GetInput() = input_val;
-			layers.Evaluate();
+		void Learn(const Eigen::Matrix<double, stacktype::input_size::value, 1>& input_val, const Eigen::Matrix<double, stacktype::output_size::value, 1>& output_val, double learning_rate) const {
+			layers.Evaluate(input_val);
 			layers.Backpropagate(learning_rate, output_val);
 		}
 
